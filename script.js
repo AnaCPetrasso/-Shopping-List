c7d46ed649023fae263773a953e5a528db7bcd40
Ana Carolina Petrasso <carolina15petrasso@gmail.com>
2023-08-14 20:05:48 -0300

    update html an js

--- script.js
+++ script.js
@@ -2,61 +2,81 @@ const $ = (selector) => document.querySelector(selector);
 
 const shoppingList = [];
 
+const createListItem = (item) => {
+  const liItem = document.createElement('li');
+  liItem.classList.add("list-item");
+  
+  const liContent = document.createTextNode(item);
+  liItem.appendChild(liContent);
+  
+  const deleteBtn = document.createElement('button');
+  deleteBtn.classList.add("btn");
+  deleteBtn.innerText = "Delete";
+  deleteBtn.addEventListener("click", () => deleteItem(item));
+  liItem.appendChild(deleteBtn);
+  
+  const updateBtn = document.createElement("button");
+  updateBtn.classList.add("btn");
+  updateBtn.innerText = "Edit";
+  updateBtn.addEventListener("click", () => updateItem(item));
+  liItem.appendChild(updateBtn);
+  
+  return liItem;
+};
+
 const addItem = () => {
-    let newItem = $("#newItemInput").value.toUpperCase();
+  const newItem = $("#newItemInput").value.trim().toUpperCase();
+  if (newItem) {
     shoppingList.push(newItem);
     createList(shoppingList);
     $("#listForm").reset();
+  }
 };
 
 const createList = (list) => {
-    $("#list").innerHTML = ""
-    list.forEach((item) => {
-        let liContent = document.createTextNode(`${item}`);
-        let liItem = document.createElement('li');
-        let deleteBtn = document.createElement('button');
-        deleteBtn.classList.add("btn");
-        deleteBtn.addEventListener("click", ()=>deleteItem(item))
-        deleteBtn.innerText = "Delete"; 
-        let updateBtn = document.createElement("button");
-        updateBtn.classList.add("btn");
-        updateBtn.innerText = "Edit";
-        updateBtn.addEventListener("click",()=>updateItem(item))
-        liItem.classList.add("list-item");
-        liItem.appendChild(liContent);
-        liItem.appendChild(deleteBtn);
-        liItem.appendChild(updateBtn);
-        $("#list").appendChild(liItem);
-    });
+  const listContainer = $("#list");
+  listContainer.innerHTML = "";
+  
+  list.forEach((item) => {
+    const liItem = createListItem(item);
+    listContainer.appendChild(liItem);
+  });
 };
-//hago la función de eliminar item
-const deleteItem = (item) =>{
-    const itemIndex = shoppingList.indexOf(item)
-    shoppingList.splice(itemIndex,1)
-    createList(shoppingList)
-    //puedo usar el filter tmb
-}
-//hago la función de editar item
-const updateItem = (item)=>{
-    let newValue = prompt("Cambio").toLocaleUpperCase()
-    let itemIndex = shoppingList.indexOf(item)
-    shoppingList[itemIndex] = newValue
-    createList(shoppingList)
-}
-//hago función para permitir que al tocar enter agregue el item
+
+const deleteItem = (item) => {
+  const itemIndex = shoppingList.indexOf(item);
+  if (itemIndex !== -1) {
+    shoppingList.splice(itemIndex, 1);
+    createList(shoppingList);
+  }
+};
+
+const updateItem = (item) => {
+  const newValue = prompt("Cambio")?.trim().toUpperCase();
+  if (newValue) {
+    const itemIndex = shoppingList.indexOf(item);
+    if (itemIndex !== -1) {
+      shoppingList[itemIndex] = newValue;
+      createList(shoppingList);
+    }
+  }
+};
+
 const handleEnterKeyPress = (event) => {
-    if (event.key === "Enter") {
-        addItem();
-    }
+  if (event.key === "Enter") {
+    event.preventDefault()
+    addItem();
+  }
 };
-//creo fución para cambiar de modo claro a  modo oscuro
-const changeMode = () =>{
-    if($("#body").getAttribute("data-theme") === "light"){
-        $("#body").setAttribute("data-theme", "dark")
-    }else{
-        $("#body").setAttribute("data-theme", "light")
-    }
-}
+
+const changeMode = () => {
+  const body = $("#body");
+  const currentTheme = body.getAttribute("data-theme");
+  body.setAttribute("data-theme", currentTheme === "light" ? "dark" : "light");
+  const modeIcon = $("#modeIcon");
+  modeIcon.innerText = currentTheme === "light" ? "☀️" : "🌑"; // Cambiar el emoji del logo
+};
+
 $("#addButton").addEventListener("click", addItem);
 $("#newItemInput").addEventListener("keydown", handleEnterKeyPress);
-$("#modeBtn").addEventListener("click",changeMode)
+$("#modeBtn").addEventListener("click", changeMode);
